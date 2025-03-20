2e622ce23098431f15d7a586f2db0f9026129c33
DanishHaji <danish.haji111@gmail.com>
2025-03-20 23:54:30 +0500

    Create 01_dicesimulator.py

---
+++ Assignment 1 Python Fundamentals/01_expressions/01_dicesimulator.py
@@ -0,0 +1,27 @@
+"""
+Problem Statement
+
+Simulate rolling two dice, three times. Prints the results of each die roll. This program is used to show how variable scope works.
+"""
+
+import random
+
+NUM_SIDES = 6
+
+def roll_dice():
+    # Rolls a two dice and return the result.
+    die1: int = random.randint(1, NUM_SIDES)
+    die2: int = random.randint(1, NUM_SIDES)
+    total: int = die1 + die2
+    print("Total of two dice:", total)
+
+def main():
+    die1: int = 10
+    print("die1 in main() starts as: " + str(die1))
+    roll_dice()
+    roll_dice()
+    roll_dice()
+    print("die1 in main() after rolling three dice: " + str(die1))
+
+if __name__ == '__main__':
+    main()
