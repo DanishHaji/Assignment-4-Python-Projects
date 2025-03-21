53974e289eddeaee77e7ee66cdcee3f23f3a5de2
DanishHaji <danish.haji111@gmail.com>
2025-03-21 17:05:12 +0500

    added some questions

--- Assignment 1 Python Fundamentals/01_expressions/05_remainder_division.py
+++ Assignment 1 Python Fundamentals/01_expressions/05_remainder_division.py
@@ -7,3 +7,13 @@ Please enter an integer to be divided: 5
 Please enter an integer to divide by: 3
 The result of this division is 1 with a remainder of 2
 '''
+def main():
+    num1 = int(input("Please enter an integer to be divided: "))
+    num2 = int(input("Please enter an integer to by divided by: "))
+    result = num1 // num2
+    remainder = num1 % num2
+
+    print("The result of this division is " + str(result) + " with a remainder of " + str(remainder))
+
+if __name__ == "__main__":
+    main()
--- Assignment 1 Python Fundamentals/01_expressions/06_rolldice.py
+++ Assignment 1 Python Fundamentals/01_expressions/06_rolldice.py
@@ -3,3 +3,21 @@ Problem Statement
 Simulate rolling two dice, and prints results of each roll as well as the total.
 '''
 
+import random
+SIDES = 6
+
+def main():
+    die1: int = random.randint(1, SIDES)
+    die2: int = random.randint(1, SIDES)
+
+    total: int = die1 + die2
+
+    print("Dice have", SIDES, "sides each.")
+    print("First die:", die1)
+    print("Second die:", die2)
+    print("Total of two dice:", total)
+if __name__ =='__main__':
+    main()
+    
+
+
