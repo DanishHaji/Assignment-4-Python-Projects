736da0d58beeeace49fded34858f094bbf62f4ea
DanishHaji <danish.haji111@gmail.com>
2025-04-04 17:03:30 +0500

    Completed 00 to 07 Projects

--- Assignment 1 Python Fundamentals/07_information_flow/05_subtract_7.py
+++ Assignment 1 Python Fundamentals/07_information_flow/05_subtract_7.py
@@ -1,6 +1,6 @@
 '''
 Problem Statement
-Fill out the subtract_seven helper function to subtract 7 from num, and fill out the main() method to call the subtract_seven helper function! If you're stuck, revisit the add_five example from lecture.
+Fill out the subtract_seven helper function to subtract 7 from num, and fill out the main() method to call the subtract_seven helper function! If you're stuck, revisit the add_five example from lecture..
 '''
 
 def subtract_seven(num):
